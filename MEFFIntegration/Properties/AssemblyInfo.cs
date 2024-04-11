bc70fd9d49722de9567295bb0b8c0011f76cac78
Enrico Favali <enrico.favali@fairmat.com>
2024-04-11 14:22:04 +0200

    [RD-FFK-NET8] Upgraded MEFIntegration to double target and removed unused references fairmat/dev/fairmatserver#3493

--- MEFFIntegration/Properties/AssemblyInfo.cs
+++ MEFFIntegration/Properties/AssemblyInfo.cs
@@ -25,16 +25,7 @@ using Mono.Addins;
 [assembly: AddinDependency("Fairmat", "1.0")]
 [assembly: AddinAuthor("Fairmat SRL")]
 [assembly: AddinDescription("Provides access to MEFF Market Data.")]
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MEFF Integration")]
 [assembly: AddinDescription("Provides access to MEFF Market Data.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fairmat SRL")]
-[assembly: AssemblyProduct("MEFF Integration")]
-[assembly: AssemblyCopyright("Copyright © Fairmat SRL 2013-2015")]
 [assembly: AssemblyTrademark("Fairmat")]
 [assembly: AssemblyCulture("")]
 
@@ -45,17 +36,4 @@ using Mono.Addins;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("576D1D97-5EB9-4F5D-9A52-EA60158BE1DE")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
 [assembly: InternalsVisibleTo("MarketDataProviders.Tests")]
